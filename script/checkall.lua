a5d8b6ef31a3dbe51b7bf2c533598e66ea3bd86f
Cloud Wu <cloudwu@gmail.com>
2017-04-07 22:30:03 +0800

    fix bom and update check script

--- script/checkall.lua
+++ script/checkall.lua
@@ -1,16 +1,20 @@
-local filepath = "../cn/localisation"
+local filepath = "../cn/localisation/english"
 local scriptpath = "."
 
 local filelist = {
+	"achievements_l_english.yml",
 	"dip_messages_l_english.yml",
 	"event_chains_l_english.yml",
 	"event_chains_l_english_tag.yml",
 	"events_2_l_english.yml",
 	"events_3_l_english.yml",
 	"events_4_l_english.yml",
+	"events_5_l_english.yml",
 	"events_l_english.yml",
+	"gamepad_indicator_text_l_english.yml",
 	"guardian_l_english.yml",
 	"guardian_l_english_tag.yml",
+	"horizonsignal_l_english.yml",
 	"l_english.yml",
 	"l_english_tag.yml",
 	"mandates_l_english.yml",
@@ -18,18 +22,28 @@ local filelist = {
 	"modifiers_2_l_english.yml",
 	"modifiers_3_l_english.yml",
 	"modifiers_l_english.yml",
+	"modifiers_utopia_l_english.yml",
+	"musicplayer_l_english.yml",
 	"name_lists_l_english.yml",
 	"pop_factions_l_english.yml",
 	"prescripted_l_english.yml",
 	"projects_2_l_english.yml",
 	"projects_3_l_english.yml",
 	"projects_4_l_english.yml",
+	"projects_5_l_english.yml",
 	"projects_l_english.yml",
+	"scripted_loc_l_english.yml",
 	"ship_sections_l_english.yml",
 	"standalone_l_english.yml",
 	"technology_l_english.yml",
+	"traditions_l_english.yml",
 	"triggers_effects_l_english.yml",
 	"tutorial_l_english.yml",
+	"unrest_l_english.yml",
+	"utopia_ascension_l_english.yml",
+	"utopia_l_english.yml",
+	"utopia_megastructures_l_english.yml",
+	"zzexceedtranslation_l_english.yml",
 }
 
 local check_text = assert(loadfile(scriptpath .. "/check.lua"))()

7d4f931b9a7e23f1b3624648fbe6c0260543d2e3
Cloud Wu <cloudwu@gmail.com>
2022-05-15 22:04:28 +0800

    update script

--- script/simpc.lua
+++ script/simpc.lua
@@ -1,8 +1,9 @@
 local path1 = "../cn/localisation/english/"
 local path2 = "../cn/localisation/simp_chinese/"
+local list = require "filelist"
 
 local function convert(filename)
-	local f,err = io.open(path2 .. filename:gsub("_english", "_simp_chinese"), "wb")
+	local f = assert(io.open(path2 .. filename:gsub("_english", "_simp_chinese"), "wb"))
 	local replace
 	for line in io.lines(path1 .. filename) do
 		if not replace then
@@ -16,80 +17,6 @@ local function convert(filename)
 
 	f:close()
 end
-
-local list = {
-"achievements_l_english.yml",
-"ai_crisis_l_english.yml",
-"ancient_relics_events_l_english.yml",
-"ancient_relics_l_english.yml",
-"apocalypse_l_english.yml",
-"aquatics_l_english.yml",
-"dip_messages_l_english.yml",
-"diplo_stances_l_english.yml",
-"distant_stars_l_english.yml",
-"dlc_recommendations_l_english.yml",
-"event_chains_l_english.yml",
-"events_2_l_english.yml",
-"events_3_l_english.yml",
-"events_4_l_english.yml",
-"events_5_l_english.yml",
-"events_6_l_english.yml",
-"events_l_english.yml",
-"federations_anniversary_l_english.yml",
-"federations_l_english.yml",
-"federations_resolution_comments_l_english.yml",
-"gamepad_indicator_text_l_english.yml",
-"horizonsignal_l_english.yml",
-"hud_tutorial_l_english.yml",
-"immediate_tutorial_l_english.yml",
-"l_english.yml",
-"leviathans_l_english.yml",
-"lithoids_l_english.yml",
-"mandates_l_english.yml",
-"marauder_l_english.yml",
-"megacorp_l_english.yml",
-"messages_l_english.yml",
-"modifiers_2_l_english.yml",
-"modifiers_3_l_english.yml",
-"modifiers_l_english.yml",
-"modifiers_utopia_l_english.yml",
-"musicplayer_l_english.yml",
-"name_lists_l_english.yml",
-"necroids_l_english.yml",
-"nemesis_content_l_english.yml",
-"nemesis_crisis_l_english.yml",
-"nemesis_custodian_l_english.yml",
-"nemesis_espionage_l_english.yml",
-"nemesis_intel_l_english.yml",
-"new_scripted_loc_l_english.yml",
-"observer_events_l_english.yml",
-"observer_l_english.yml",
-"pop_factions_l_english.yml",
-"plantoids_l_english.yml",
-"prescripted_l_english.yml",
-"projects_2_l_english.yml",
-"projects_3_l_english.yml",
-"projects_4_l_english.yml",
-"projects_5_l_english.yml",
-"projects_l_english.yml",
-"scripted_loc_l_english.yml",
-"ship_browser_l_english.yml",
-"ship_sections_l_english.yml",
-"situations_l_english.yml",
-"situations_victor_l_english.yml",
-"social_gui_l_english.yml",
-"standalone_l_english.yml",
-"synthetic_dawn_events_l_english.yml",
-"technology_l_english.yml",
-"traditions_l_english.yml",
-"triggers_effects_l_english.yml",
-"tutorial_l_english.yml",
-"tutorial_overview_l_english.yml",
-"unrest_l_english.yml",
-"utopia_ascension_l_english.yml",
-"utopia_l_english.yml",
-"utopia_megastructures_l_english.yml",
-}
 
 for _,file in ipairs(list) do
 	convert(file)
